2b0e66a63d3777f1ea0b88f1e66d1828dfc911b1
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2026-01-03 00:23:59 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -25,11 +25,11 @@ fi
 clear
 apt install jq curl -y
 ###DOM#AIN=installer.my.id
-DOMAIN=profesorhokage.my.id
+DOMAIN=hokage.web.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c6)
-SUB_DOMAIN=${sub}.profesorhokage.my.id
-CF_ID=dedenirwansyah90@gmail.com
-CF_KEY=2affde1c2638841171d852fd8c60987f57c73
+SUB_DOMAIN=${sub}.hokage.web.id
+CF_ID=faridaumiabi@gmail.com
+CF_KEY=ab8d0901acb186291e58f21359d07e7b847dc
 set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
